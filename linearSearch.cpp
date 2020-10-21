5e40abc7734752daafd9d0fb02c98dd5fd2e1ad5
Jamyang Lotus <lotusjamyang@gmail.com>
2020-10-21 11:37:52 +0530

    iterative approach of linear search

--- linearSearch.cpp
+++ linearSearch.cpp
@@ -1,16 +1,23 @@
 //implementing linear search in an unsorted array
-//It is an example of subtractive recursive/iterative algorithm
-//as after every iteration size of an array decreases by 1
+//It is an iterative algorithm
+
 #include<iostream>
 #include<stdlib.h>
 using namespace std;
 
 //time complexity O(n)
-//searches recursively
-int linearSearch(int a[], int low, int high, int key){
-  if (high<low){return -1;}
-  if(a[low]==key){return low;}
-  return linearSearch(a,low+1,high,key);
+//searches iteratively
+int linearSearch(int a[], int size,int key)
+{
+  for(int i = 0; i < n; i++)
+  {
+    if(a[i] == key)
+    {
+      //formatted output
+      printf("%d is found at %d position",key,i+1);
+      break;
+    }
+  }
 }
 
 int main(){
@@ -24,7 +31,7 @@ int main(){
   for(int i=0;i<size;i++){cin>>a[i];}
   cout<<"\nEnter the element to be searched : ";
   cin>>key;
-  int result=linearSearch(a,0,9,key);
+  int result=linearSearch(a,size,key);
   if(result==-1){cout<<"No such element";}
   else cout<<"Position of "<<key<<" is "<<result+1;
   return 0;
